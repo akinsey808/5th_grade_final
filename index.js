b2f455a6b248a06b27d8ceb269bda71149195015
Anabel Kinsey <akinsey@gmail.com>
2016-05-16 20:14:27 -1000

    more files

---
+++ index.js
@@ -0,0 +1,15 @@
+/*
+var p1 = document.getElementById('p1');
+var   rectObject = p1.getBoundingClientRect();
+console.log (rectObject);
+console.log(window.innerWidth, window.innerHeight);
+console.log(rectObject.left / window.innerWidth, rectObject.top / window.innerHeight);
+
+var newTop = Math.random() - 0.5;
+if (newTop < 0) {
+  newTop = 0;
+}
+//if (newTop > window.)
+p1.style.top = (Math.random() - 0.5) + 'vw';
+console.log(p1.style.top);
+*/
